b94bb7c2a5295346e8ab75561a4bf969cd819f4e
bgale <bgale@outlook.com>
2017-01-20 15:56:15 -0400

    routing structure, stubbed route components

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 
 export default class App extends Component {
   render(){
     return (
-      <h1>Hellow World</h1>
+      <Router history={hashHistory}>
+        <Route path="/">
+          <IndexRoute component={Chart} />
+          <Route path="/settings" component={Settings} />
+        </Route>
+        <Route path="/setup" component={Setup}/>
+      </Router>
     )
   }
 }
+
+const Chart = () => (<h1>CHART</h1>)
+const Settings = () => (<h1>SETTINGS</h1>)
+const Setup = () => (<h1>SETUP</h1>)
