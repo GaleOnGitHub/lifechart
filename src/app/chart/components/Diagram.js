b6e4c5b7dae02a7de562b139c3c4623a1ee62a19
bgale <bgale@outlook.com>
2017-01-22 02:24:41 -0400

    added some error messages to Diagram

--- src/app/chart/components/Diagram.js
+++ src/app/chart/components/Diagram.js
@@ -1,10 +1,32 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
+import { Link } from 'react-router'
 
-const Diagram = ({ageInUnits,lifeInUnits}) => (
-  <div>
-    {ageInUnits},{lifeInUnits}
-  </div>
-)
+class Diagram extends Component{
+  render(){
+    const {ageInUnits, lifeInUnits } = this.props
+    if(ageInUnits > lifeInUnits){ //Older than expected
+      return (
+        <h2>
+          You've' lived longer than this life expectancy!<br/>
+          Try updating your <Link to="/settings">settings</Link>.
+        </h2>
+      )
+    }
+    if(ageInUnits < 0){ //Negative age
+      return (
+        <h2>
+          Wow! Looks like you were born in the future.<br/>
+          Try updating your <Link to="/settings">settings</Link>.
+        </h2>
+      )
+    }
+    return (
+      <div>
+        {ageInUnits},{lifeInUnits}
+      </div>
+    )
+  }
+}
 
 Diagram.PropTypes = {
   ageInUnits: PropTypes.number,
