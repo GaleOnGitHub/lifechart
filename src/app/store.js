499b2677e1d4141957d309528cfd321a57f350da
bgale <bgale@outlook.com>
2017-01-24 02:13:22 -0400

    added session storage saving and loading

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,24 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import chartApp from './chart/reducers'
+import { UPDATE, update } from './chart/actions'
 
-export const store = createStore(chartApp);
+//initial state
+const getInitialState = () => {
+  const save = sessionStorage.getItem('chart')
+  const chart = JSON.parse(save)
+  
+  return {chart}
+}
+
+//middleware
+const saveToSessionStorage = store => next => action => {
+  const result = next(action)
+  if(result.type = UPDATE){ //only save on chart update
+    const chart = store.getState().chart
+    const save = JSON.stringify(chart)
+    sessionStorage.setItem('chart', save)
+  }
+  return result
+}
+
+export const store = createStore(chartApp, getInitialState(), applyMiddleware(saveToSessionStorage));
