0c1fb9508ed452599c56b671288638f849a2d465
bgale <bgale@outlook.com>
2017-01-28 13:24:25 -0400

    settings button

--- src/app/pages/layouts/MainLayout.js
+++ src/app/pages/layouts/MainLayout.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router'
 
-const MainLayout = ({title, children}) => (
+const MainLayout = ({title, children, path = null}) => (
   <div className="main-layout">
     <header className="topbar">
       <div className="container">
+        { path === '/settings' ? (
+          <Link className='nav-button' to='/chart'>Back</Link>
+        ):(
+          <Link className='nav-button' to='/settings'>Settings</Link>
+        )}
         <div className="topbar-title">
           <span className="logo">Lifechart</span>
           <h1>{title}</h1>
