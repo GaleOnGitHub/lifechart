6492052ec6cb70277ac5b602905ecc9c1cc56e54
bgale <bgale@outlook.com>
2017-01-31 14:32:46 -0400

    load url data to redirect if not valid

--- src/app/middleware/routing.js
+++ src/app/middleware/routing.js
@@ -1,9 +1,16 @@
 import { store } from '../store'
 import { update } from '../chart/actions'
 
-export const loadUrlData = (nextState,replace) => {
+export const loadUrlData = (nextState, replace) => {
   const { dob, lifespan } = nextState.params
-  store.dispatch(update(dob,lifespan,"Custom","MONTHS"))
+  const reDate = /^(19|20)[0-9]{2}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/ //yyyy-mm-dd
+  const reAge = /^(100|[0-9][1-9])$/ //age 01 to 100
+  if(reDate.test(dob) && reAge.test(lifespan)){
+    store.dispatch(update(dob,lifespan,"Custom","YEARS"))
+    replace('/chart')
+  }else{
+    replace('/')
+  }
 }
 
 export const redirectNoData = (nextState, redirect) => {
