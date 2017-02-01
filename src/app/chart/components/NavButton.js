4d2cdc178973059a2d181c16d1999a5238bbbcd9
bgale <bgale@outlook.com>
2017-02-01 00:09:31 -0400

    removed a console log

--- src/app/chart/components/NavButton.js
+++ src/app/chart/components/NavButton.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router'
 class NavButton extends Component {
     render(){
         const path = window.location.hash.split('/')[1] 
-        console.log(path)
         if(path === 'settings'){
             return (<Link className="nav-button" to="/chart">Back</Link>)
         }else{
